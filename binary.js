a2ceff97dafd370e7f7ae60e870aae1cc1c12f14
Irene Sarigu <ireaurora@gmail.com>
2020-05-24 20:38:52 +0100

    Update binary.js

--- binary.js
+++ binary.js
@@ -1,1 +1,10 @@
-
+function testResults (form) {
+  var TestVar = form.inputbox.value;
+  if ((TestVar +'').indexOf('1') >-1) { 
+    alert("Your decimal number is : " + parseInt(TestVar, 2)); 
+  }else if(TestVar !== parseInt(TestVar, 10)){
+    alert("Please only insert numbers");
+  }else{
+    alert('Please insert a binary number');
+  }
+}
